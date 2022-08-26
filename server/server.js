de81cb4831ad5d15c2973a3b4be851abb1d17500
dallasmay <dmmay9@gmail.com>
2022-08-26 16:21:23 -0600

    added express and dotenv, set up basic server file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
+require("dotenv").config();
+const { SERVER_PORT } = process.env;
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running on port ${SERVER_PORT}`);
+});
+
 // DATABASE TABLES
 
 // CREATE TABLE users (
